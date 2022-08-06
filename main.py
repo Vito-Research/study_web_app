5651e8f314de71785889455efdda2ed2eb9e3d19
Grant Randa <48031522+grantranda@users.noreply.github.com>
2022-08-06 11:51:45 -0400

    Update grammar

--- main.py
+++ main.py
@@ -7,23 +7,27 @@ from fitbit import hrv
 st.image("Vito.png")
 
 st.header("Vito Study")
-
-st.subheader("A study aiming to detect infection in realtime using smartwatch data")
+st.subheader("A study aiming to detect infection in real time using smartwatch data")
 
 link = '[More Information](https://vitovitals.org)'
 st.markdown(link, unsafe_allow_html=True)
 st.image("Promo.png")
 st.write("""
+This form is designed to describe the study’s goals, the overall nature of the study, the risks involved, 
+and the time commitment necessary. This study is designed to measure the risk of COVID-19 using physiological data 
+commonly found on smartwatches. 
 
-This form is designed to describe the study’s goals, the overall nature of the study, risks involved, and the amount of time commitment necessary. 
-This study is designed to measure the risk of Covid-19 using physiological data commonly found on smartwatches.   
+You are being asked to participate in this study because you are a student at the University of North Florida who has 
+a smartwatch, are willing to download the app used to conduct the study, and are willing to report a COVID-19 diagnosis 
+and/or log symptoms upon notification from the app. Please keep in mind that this is voluntary, meaning you may only 
+participate if you desire to. Participating in this study requires ten minutes of setup time and roughly five 
+minutes per month of symptom logging. Benefits of this study include reducing future spread of viral infections via 
+real-time alerting of infection, encouraging further testing of infectious diseases, and conducting a more in-depth 
+analysis of one’s health via a medical professional’s supervision. 
 
-You are being asked to participate in this study because you are a student at the University of North Florida who has a smartwatch, are willing to download the app used to conduct the study and are willing to report COVID-19 diagnosis and or log symptoms upon notification from the app, please keep in mind this is voluntary, meaning you may only participate if you desire to. 
-Participating in this study requires ten minutes of set up time, and roughly five minutes per month of symptom logging. 
-Benefits of this study include reducing future spread of viral infections via real time alerting of infection, encouraging further testing of infectious diseases, and making more in-depth analysis of one’s health via a medical professional’s supervision.   
-
-There are no known risks to participating in this study. 
-The data collected in this study is collected anonymously, the researchers nor anyone else will know the origin of the data other than the fact that it came from an authorized participant via the app, thus the data is not linked to participants. 
+There are no known risks to participating in this study. The data used in this study are collected anonymously, 
+and neither the researchers nor anyone else will know the origin of the data other than the fact that they came from an 
+authorized participant via the app, thus the data are not linked to participants.
 """)
 
 st.subheader("1. Press the \"Authorize with Fitbit\"")
