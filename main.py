6c95cd8130a29ff7db1dd96cb6c9998638646d35
Grant Randa <48031522+grantranda@users.noreply.github.com>
2022-08-13 11:05:05 -0400

    Add main() to main.py

--- main.py
+++ main.py
@@ -3,79 +3,85 @@ import streamlit as st
 import fire
 from fitbit import *
 
-fire.init("serviceAccount.json")
 
-st.image("Vito.png")
-st.header("Vito Study")
-st.subheader("A study aiming to detect infection in real time using smartwatch data")
+def main():
+    fire.init("serviceAccount.json")
 
-link = '[More Information](https://vitovitals.org)'
-st.markdown(link, unsafe_allow_html=True)
-st.image("Promo.png")
-st.write("""
-This form is designed to describe the study’s goals, the overall nature of the study, the risks involved, 
-and the time commitment necessary. This study is designed to measure the risk of COVID-19 using physiological data 
-commonly found on smartwatches. 
+    st.image("Vito.png")
+    st.header("Vito Study")
+    st.subheader("A study aiming to detect infection in real time using smartwatch data")
 
-You are being asked to participate in this study because you are a student at the University of North Florida who has 
-a smartwatch, are willing to download the app used to conduct the study, and are willing to report a COVID-19 diagnosis 
-and/or log symptoms upon notification from the app. Please keep in mind that this is voluntary, meaning you may only 
-participate if you desire to. Participating in this study requires ten minutes of setup time and roughly five 
-minutes per month of symptom logging. Benefits of this study include reducing future spread of viral infections via 
-real-time alerting of infection, encouraging further testing of infectious diseases, and conducting a more in-depth 
-analysis of one’s health via a medical professional’s supervision. 
+    link = '[More Information](https://vitovitals.org)'
+    st.markdown(link, unsafe_allow_html=True)
+    st.image("Promo.png")
+    st.write("""
+    This form is designed to describe the study’s goals, the overall nature of the study, the risks involved, 
+    and the time commitment necessary. This study is designed to measure the risk of COVID-19 using physiological data 
+    commonly found on smartwatches. 
+    
+    You are being asked to participate in this study because you are a student at the University of North Florida who 
+    has a smartwatch, are willing to download the app used to conduct the study, and are willing to report a COVID-19 
+    diagnosis and/or log symptoms upon notification from the app. Please keep in mind that this is voluntary, 
+    meaning you may only participate if you desire to. Participating in this study requires ten minutes of setup time 
+    and roughly five minutes per month of symptom logging. Benefits of this study include reducing future spread of 
+    viral infections via real-time alerting of infection, encouraging further testing of infectious diseases, 
+    and conducting a more in-depth analysis of one’s health via a medical professional’s supervision. 
+    
+    There are no known risks to participating in this study. The data used in this study are collected anonymously, 
+    and neither the researchers nor anyone else will know the origin of the data other than the fact that they came 
+    from an authorized participant via the app, thus the data are not linked to participants.
+    """)
 
-There are no known risks to participating in this study. The data used in this study are collected anonymously, 
-and neither the researchers nor anyone else will know the origin of the data other than the fact that they came from an 
-authorized participant via the app, thus the data are not linked to participants.
-""")
+    st.subheader("Upload Data")
+    st.markdown("""
+        1. Click \"Authorize with Fitbit\"
+        2. If prompted, enter your login information
+        3. Once redirected to Vito's website, copy the URL
+        4. Paste the URL into the text box below
+    """)
 
-st.subheader("Upload Data")
-st.markdown("""
-    1. Click \"Authorize with Fitbit\"
-    2. If prompted, enter your login information
-    3. Once redirected to Vito's website, copy the URL
-    4. Paste the URL into the text box below
-""")
+    link = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=2389P9&redirect_uri=https%3A%2F%2Fvitovitals.org&scope=heartrate%20sleep%20oxygen_saturation%20respiratory_rate%20temperature&expires_in=604800"
+    st.markdown(f'[Authorize with Fitbit]({link})', unsafe_allow_html=True)
+    st.caption(link)
 
-link = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=2389P9&redirect_uri=https%3A%2F%2Fvitovitals.org&scope=heartrate%20sleep%20oxygen_saturation%20respiratory_rate%20temperature&expires_in=604800"
-st.markdown(f'[Authorize with Fitbit]({link})', unsafe_allow_html=True)
-st.caption(link)
+    response_container = st.container()
+    fitbit_response = response_container.text_input("Enter Response From Fitbit Authorization")
 
-response_container = st.container()
-fitbit_response = response_container.text_input("Enter Response From Fitbit Authorization")
+    preview = st.expander("Data Preview")
+    preview_placeholder = preview.empty()
+    preview_placeholder.write("No data to display")
 
-preview = st.expander("Data Preview")
-preview_placeholder = preview.empty()
-preview_placeholder.write("No data to display")
+    fitbit_data = FitbitData()
 
-fitbit_data = FitbitData()
+    if fitbit_response != "":
+        try:
+            parsed = fitbit_response.split("#access_token=")[1]
+            token = parsed.split("&user_id")[0]
+            user_id = parsed.split("&user_id=")[1].split("&")[0]
 
-if fitbit_response != "":
-    try:
-        parsed = fitbit_response.split("#access_token=")[1]
-        token = parsed.split("&user_id")[0]
-        user_id = parsed.split("&user_id=")[1].split("&")[0]
+            preview_container = preview_placeholder.container()
+            preview_container.markdown(f"**User ID:**  \n{user_id}")
+            preview_container.markdown(f"**Access Token:**  \n{token}")
 
-        preview_container = preview_placeholder.container()
-        preview_container.markdown(f"**User ID:**  \n{user_id}")
-        preview_container.markdown(f"**Access Token:**  \n{token}")
+            fitbit_data.heart_rate = get_heart_rate(token, user_id, "2020-01-01", "2022-01-08")
+            fitbit_data.heart_rate_variability = get_heart_rate_variability(token, user_id, "2020-01-01", "2022-01-08")
+            fitbit_data.breathing_rate = get_breathing_rate(token, user_id, "2020-01-01", "2022-01-08")
+            fitbit_data.oxygen_saturation = get_oxygen_saturation(token, user_id, "2020-01-01", "2022-01-08")
 
-        fitbit_data.heart_rate = get_heart_rate(token, user_id, "2022-01-01", "2022-08-01")
-        fitbit_data.heart_rate_variability = get_heart_rate_variability(token, user_id, "2022-01-01", "2022-08-01")
-        fitbit_data.breathing_rate = get_breathing_rate(token, user_id, "2022-01-01", "2022-08-01")
-        fitbit_data.oxygen_saturation = get_oxygen_saturation(token, user_id, "2022-01-01", "2022-08-01")
+            preview_container.write(fitbit_data.heart_rate)
+            preview_container.write(fitbit_data.heart_rate_variability)
+            preview_container.write(fitbit_data.breathing_rate)
+            preview_container.write(fitbit_data.oxygen_saturation)
+        except IndexError:
+            response_container.error("Invalid input")
 
-        preview_container.write(fitbit_data.heart_rate)
-        preview_container.write(fitbit_data.heart_rate_variability)
-        preview_container.write(fitbit_data.breathing_rate)
-        preview_container.write(fitbit_data.oxygen_saturation)
-    except IndexError:
-        response_container.error("Invalid input")
+    col1, col2 = st.columns([1, 6])
+    if col1.button("Submit") and not fitbit_data.is_empty():
+        with col2:
+            with st.spinner("Uploading data..."):
+                fire.upload_fitbit_data(fitbit_data)
+        st.success("Data uploaded successfully!")
 
-col1, col2 = st.columns([1, 6])
-if col1.button("Submit") and not fitbit_data.is_empty():
-    with col2:
-        with st.spinner("Uploading data..."):
-            fire.upload_fitbit_data(fitbit_data)
-    st.success("Data uploaded successfully!")
+
+if __name__ == "__main__":
+    main()
