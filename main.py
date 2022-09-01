e42c74ae92ea8ddeb0b5b5a1e3d82aadb6d13c3e
Andreas Ink <andreasink17@gmail.com>
2022-09-01 00:27:50 -0400

    rm empty func

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def main():
           
 
     col1, col2 = st.columns([1, 6])
-    if col1.button("Submit") and not fitbit_data.is_empty():
+    if col1.button("Submit"):
         with col2:
             with st.spinner("Uploading data..."):
                 fire.upload_fitbit_data(fitbit_data)
